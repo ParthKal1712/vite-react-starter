0f3b1c681cecd46e07e5a20a4185749ffeb24622
Parth Kalra <parth.kalra1712@gmail.com>
2024-10-18 11:32:01 -0600

    Installed and Configured Basic Vite PWA

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import createJiti from "jiti";
 import { fileURLToPath } from "node:url";
 import * as path from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 const jiti = createJiti(fileURLToPath(import.meta.url));
 
@@ -13,7 +14,40 @@ jiti("./src/env/server.ts");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteReact(), TanStackRouterVite()],
+  plugins: [
+    viteReact(),
+    VitePWA({
+      manifest: {
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      registerType: "autoUpdate",
+    }),
+    TanStackRouterVite(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
